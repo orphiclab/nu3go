99c1b4256b6625483a48dbe3481b957b3316e2c4
Avishka Rathnayake <avishka@Avishkas-MacBook-Pro.local>
2026-03-03 09:45:20 +0530

    fix: resolve typeorm paths properly for production dist execution

--- nu3go-api/src/database/typeorm.config.ts
+++ nu3go-api/src/database/typeorm.config.ts
@@ -7,8 +7,8 @@ dotenv.config({ path: path.resolve(__dirname, '../.env.development') });
 export const AppDataSource = new DataSource({
     type: 'postgres',
     url: process.env.DATABASE_URL,
-    entities: [__dirname + '/modules/**/entities/*.entity{.ts,.js}'],
-    migrations: [__dirname + '/database/migrations/*{.ts,.js}'],
+    entities: [path.join(__dirname, '/../modules/**/entities/*.entity{.ts,.js}')],
+    migrations: [path.join(__dirname, '/migrations/*{.ts,.js}')],
     logging: process.env.NODE_ENV !== 'production',
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
